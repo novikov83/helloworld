4d161fb68ec25f26d0199a1975e4c747019c7a4c
Roman <roman@MacBook-2.local>
2022-04-05 21:33:58 +0300

    First commit

---
+++ helloworld_cli.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "config.h"
+
+int main(int, char**)
+{
+	std::cout << "Version: " << PROJECT_VERSION << std::endl;
+	std::cout << "Hello, World!" << std::endl;
+
+	return 0;
+}
+
